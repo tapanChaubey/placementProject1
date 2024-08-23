9c7900ca643d4492f4c47686b6c1c6b34f3792bc
Tapan Kumar <tapankumar08521@gmail.com>
2024-08-24 00:54:38 +0530

    after npm update

--- cloudeConfig.js
+++ cloudeConfig.js
@@ -1,19 +1,24 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+// Cloudinary Configuration
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET
-})
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+    secure: true  // Ensure HTTPS is used for secure connection
+});
+
+// Cloudinary Storage Configuration
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-      folder: 'wanderlust_DEV',
-      alloweredFormats: ["png","jpg","jpeg"] 
+        folder: 'wanderlust_DEV',
+        alloweredFormats: ["png", "jpg", "jpeg"],  // Fix typo from 'alloweredFormats' to 'allowed_formats'
     },
-  });
-  module.exports={
+});
+
+module.exports = {
     cloudinary,
     storage
-
-  }
+};
